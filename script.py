dc171e0537ee356014a0a1985b68129794e1a152
Uzbeksagur <168723767+Uzbeksagur@users.noreply.github.com>
2025-01-09 17:16:25 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,46 @@
 from playwright.sync_api import sync_playwright
+import schedule
+import time
+from datetime import datetime
+from pybit.unified_trading import HTTP
+from pybit.unified_trading import WebSocket
+import requests
+import os
 from keep_alive import keep_alive
 keep_alive()
+
+class Row:
+  def __init__(self, symbol, time, rate):
+    self.symbol = symbol
+    self.time = time
+    self.rate = rate
+
+api_key = os.environ.get("key")
+api_secret = os.environ.get("secret")
+
+token = os.environ.get("token")
+chat_id = os.environ.get("chat")
+
+# Initialize HTTP session and WebSocket
+session = HTTP(demo=True, api_key=api_key, api_secret=api_secret)
+
+def handle_wallet(message):
+    avi = message.get('totalAvailableBalance', 'Valoare indisponibilă')
+    mar = message.get('totalMarginBalance', 'Valoare indisponibilă')
+    return(f'Available: {round(float(avi), 2)}, WithMargin: {round(float(mar), 2)}')
+
+# Function to send Messages in Telegram
+def sendMessage():
+    try:
+        wallet = session.get_wallet_balance(accountType="UNIFIED")
+        message = wallet["result"]["list"][0]
+        try: 
+            url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={handle_wallet(message)}"
+            requests.get(url)
+        except Exception as e:
+            print(f"Error seting Wallet Message: {e}")
+    except Exception as e:
+        print(f"Error sending Wallet Message: {e}")
 
 # Scriptul Playwright
 def fetch_table_data():
@@ -20,28 +60,124 @@ def fetch_table_data():
         page.click("table thead tr th:nth-child(5)")
         page.wait_for_timeout(1000)  # Pauză mică pentru a permite sortarea
 
-        # Extrage datele din prima linie, coloanele 1, 4 și 5
-        contract_name = page.locator("table tbody tr:nth-child(2) td:nth-child(1)").inner_text()
-        estimated_funding_rate = page.locator("table tbody tr:nth-child(2) td:nth-child(4)").inner_text()
-        current_upper_lower_limit = page.locator("table tbody tr:nth-child(2) td:nth-child(5)").inner_text()
+        negative = Row(page.locator("table tbody tr:nth-child(2) td:nth-child(1)").inner_text(), page.locator("table tbody tr:nth-child(2) td:nth-child(4)").inner_text(), page.locator("table tbody tr:nth-child(2) td:nth-child(5)").inner_text())
 
-        print("Cea mai profitabila:")
-        print(f"Contract Name: {contract_name}, Estimated Funding Rate: {estimated_funding_rate}, Upper/Lower Limit: {current_upper_lower_limit}")
+        # Apasă pe al cincilea <th> pentru a sorta tabelul
+        page.click("table thead tr th:nth-child(5)")
+        page.wait_for_timeout(1000)  # Pauză mică pentru a permite sortarea
+
+        positive = Row(page.locator("table tbody tr:nth-child(2) td:nth-child(1)").inner_text(), page.locator("table tbody tr:nth-child(2) td:nth-child(4)").inner_text(), page.locator("table tbody tr:nth-child(2) td:nth-child(5)").inner_text())
 
         # Apasă pe al patrulea <th> pentru a sorta tabelul
         page.click("table thead tr th:nth-child(4)")
         page.wait_for_timeout(1000)  # Pauză mică pentru a permite sortarea
 
-        # Extrage datele din prima linie, coloanele 1, 4 și 5 (după sortare pe coloana 4)
-        contract_name = page.locator("table tbody tr:nth-child(2) td:nth-child(1)").inner_text()
-        estimated_funding_rate = page.locator("table tbody tr:nth-child(2) td:nth-child(4)").inner_text()
-        current_upper_lower_limit = page.locator("table tbody tr:nth-child(2) td:nth-child(5)").inner_text()
-
-        print("\nCea mai apropiata:")
-        print(f"Contract Name: {contract_name}, Estimated Funding Rate: {estimated_funding_rate}, Upper/Lower Limit: {current_upper_lower_limit}")
+        last = Row(page.locator("table tbody tr:nth-child(2) td:nth-child(1)").inner_text(), page.locator("table tbody tr:nth-child(2) td:nth-child(4)").inner_text(), page.locator("table tbody tr:nth-child(2) td:nth-child(5)").inner_text())
 
         # Închide browserul
         browser.close()
 
-# Rulează funcția
-fetch_table_data()
+        if(positive.time == last.time == negative.time):
+           negativeRate = float(negative.rate.replace('%', '').replace('-', ''))
+           positiveRate = float(positive.rate.replace('%', ''))
+           if(positiveRate <= negativeRate):
+               return negative
+           else:
+               return positive
+        elif (positive.time == last.time != negative.time):
+            return positive
+        elif (negative.time == last.time != positive.time):
+            return negative
+        else:
+            return last
+
+# Function to open a position with buy/sell orders
+def open_position(price, symbol, side): 
+    qty = 200/price
+    takeProfit = 0.003
+    stopLoss = 0.075
+    # buy = 1
+    idx = 1
+    try:
+        stopPrice = price * (1 - stopLoss)
+        takePrice = price * (1 + takeProfit)
+        if side == 'Buy':
+            idx = 1
+        elif side == 'Sell':
+            idx = 2
+    except Exception as e:
+        print(f"Error setting variables: {e}")
+
+    try:
+        # Place Buy Order
+        order = session.place_order(
+            category="linear",
+            symbol=symbol,
+            side=side,
+            orderType="Limit",
+            qty=round(qty),
+            price=price,
+            stopLoss=stopPrice,
+            takeProfit=takePrice,
+            positionIdx=idx,
+            tpslMode='Partial'
+        )
+    except Exception as e:
+        print(f"Error placing BUY order: {e}")
+
+    try:
+        sendMessage()
+    except Exception as e:
+        print(f"Error send Message: {e}")
+
+# Get the latest price for the symbol
+def getPrice(symbol):
+    tiker = session.get_tickers(
+        category="linear",
+        symbol=symbol,
+    )
+    price = tiker['result']['list'][0]['lastPrice']
+    return float(price)
+
+# Cancel all open orders for the symbol
+def closeOrders():
+    session.cancel_all_orders(
+        category="linear",
+        orderFilter='Order',
+        settleCoin="USDT"
+    )
+
+# Reopen buy/sell orders after closing existing ones
+def reopen(symbol, side):
+    try:
+        closeOrders()
+    except Exception as e:
+        print(f"Error closing orders: {e}")
+    
+    try:
+        price = float(getPrice(symbol))
+        open_position(price, symbol, side)
+    except Exception as e:
+        print(f"Error reopening orders: {e}")
+
+def verify():
+    best = fetch_table_data()
+    fundingTime = datetime.strptime(best.time, '%Y-%m-%d %H:%M:%S').hour
+    timeCurrent = datetime.utcnow().hour
+    if fundingTime == 0: fundingTime = 24
+    side = 'Buy'
+    if(fundingTime == (timeCurrent + 2)):
+        rate_val = float(best.rate[:-1])
+        if(rate_val > 0.25):
+            side = 'Sell'
+            reopen(best.symbol, side)
+        elif(rate_val < -0.25):
+            side = 'Buy'
+            reopen(best.symbol, side)
+
+# Programează funcția să ruleze la începutul fiecărei ore
+schedule.every().hour.at(":58").do(verify)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)  # Așteaptă 1 secundă între verificări

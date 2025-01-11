35c868636e4e39c87ddaa61cc4e3156ea854ac2a
Uzbeksagur <168723767+Uzbeksagur@users.noreply.github.com>
2025-01-11 19:37:21 +0200

    Update script.py

--- script.py
+++ script.py
@@ -45,7 +45,7 @@ def sendMessage():
 def fetch_table_data():
     with sync_playwright() as p:
         # Pornește browserul headless
-        browser = p.firefox.launch(headless=True)  # Setează la True dacă nu vrei să vezi browserul
+        browser = p.chromium.launch()  # Setează la True dacă nu vrei să vezi browserul
         page = browser.new_page()
 
         # Deschide URL-ul
